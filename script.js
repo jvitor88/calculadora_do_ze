d5f77f76dc38e1a49c769e8c80efa9696f7b3f31
jvitor88 <80867252+jvitor88@users.noreply.github.com>
2022-05-30 09:42:59 -0300

    Inserçao_comentarios

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//Função Principal (Para calcular a cerveja mais barata).
 function calcular() {
+  //Pega a div que será o resultado.
   let resultado = document.getElementById('resp')
   resultado.innerHTML = ''
+  //Array com objetos, recebendo os inputs e já calculando o valor/ml.
   let cervejas = [
     {nome: "Litrão", valor: (Number(document.getElementById('litrao').value)/1000)},
     {nome: "Long Neck", valor: (Number(document.getElementById('long').value)/330)},
@@ -11,6 +14,7 @@ function calcular() {
     {nome: "Latinha", valor: (Number(document.getElementById('latinha').value)/269)},
     {nome: "Outros", valor: (Number(document.getElementById('outros').value)/Number(document.getElementById('mloutros').value))},
   ]
+  //Função sort com funcão anônima para definir o menor valor.
   cervejas.sort(function(a, b) {
     if(a.valor < b.valor) {
       return -1
@@ -19,13 +23,16 @@ function calcular() {
         return true
       }
       })
+  //Iteração para pegar apenas o nome das cervejas já ordenadas do menor para o maior
   var dados = [];
   for(let elemento of cervejas){
     if (elemento.valor != 0)
       dados.push(elemento.nome);
   }
+  //Print da opção mais barata (primeiro da lista ordenada pelo menor)
   resultado.innerHTML = `A melhor opção é: <strong>${dados[0]}</strong>`
     }
+  //Função para limpar os campos
 function limpar(){
   document.getElementById('resp').value = ''
   document.getElementById('litrao').value = ''
@@ -37,5 +44,5 @@ function limpar(){
   document.getElementById('latinha').value = ''
   document.getElementById('mloutros').value = ''
   document.getElementById('outros').value = ''
-  let resultado = document.getElementById('resp').innerHTML = ''
+  document.getElementById('resp').innerHTML = ''
 }
